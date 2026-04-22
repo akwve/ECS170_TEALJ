cc596065a7085e9f6a30c4f2bda7f687ceb3a05a
akwve <ay.suganuma@gmail.com>
2026-04-22 16:44:17 -0700

    fixed result loading

--- ECS170_Spring_2026_Source_Code_Template/script/stage_2_script/script_load_result.py
+++ ECS170_Spring_2026_Source_Code_Template/script/stage_2_script/script_load_result.py
@@ -5,7 +5,7 @@ if 1:
     result_obj.result_destination_folder_path = '../../result/stage_2_result/'
     result_obj.result_destination_file_name = 'MLP_prediction_result'
 
-    for fold_count in [1, 2, 3, None]:
+    for fold_count in [1, 2, 3]:
         result_obj.fold_count = fold_count
         result_obj.load()
         print('Fold:', fold_count, ', Result:', result_obj.data)

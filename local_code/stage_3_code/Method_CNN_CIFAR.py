b0e5a3449723ab5d5e0296c5bf8dd4f9b57b3776
timothy-2183 <timothyhansh@gmail.com>
2026-05-18 23:16:30 -0700

    Super Big Cifar Model, needs to be run and adjusted properly

---
+++ local_code/stage_3_code/Method_CNN_CIFAR.py
@@ -0,0 +1,123 @@
+'''
+Concrete MethodModule class for a specific learning MethodModule
+'''
+
+# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
+# License: TBD
+
+from local_code.base_class.method import method
+from local_code.stage_3_code.Evaluate_Accuracy import Evaluate_Accuracy
+import torch
+from torch import nn
+import numpy as np
+
+
+class Method_CNN_CIFAR(method, nn.Module):
+    data = None
+    # it defines the max rounds to train the model
+    max_epoch = 30
+    # it defines the learning rate for gradient descent based optimizer for model learning
+    learning_rate = 1e-3
+
+    # it defines the the MLP model architecture, e.g.,
+    # how many layers, size of variables in each layer, activation function, etc.
+    # the size of the input/output portal of the model architecture should be consistent with our data input and desired output
+    def __init__(self, mName, mDescription):
+        method.__init__(self, mName, mDescription)
+        nn.Module.__init__(self)
+        self.conv1 = nn.Conv2d(3,32,kernel_size= 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu1 = nn.ReLU()
+        
+        self.conv2 = nn.Conv2d(32,32,kernel_size= 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.relu2 = nn.ReLU()
+        # Double the convolution layers before each pooling compared to ORL
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.conv3 = nn.Conv2d(32,64,3,padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.relu3 = nn.ReLU()
+
+        self.conv4 = nn.Conv2d(64,64,3,padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.relu4 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2)
+        
+        self.fcmini = nn.Linear(128*8*8,512)
+        self.fc1 = nn.Linear(64*8*8,512)
+        self.relu5 = nn.ReLU()
+
+        self.drop = nn.Dropout(0.5)
+
+        self.fc2 = nn.Linear(512,10)
+
+    # it defines the forward propagation function for input x
+    # this function will calculate the output layer by layer
+    def forward(self, x):
+        h = self.relu1(self.bn1(self.conv1(x)))
+        h = self.relu2(self.bn2(self.conv2(h)))
+        h = self.pool1(h)
+
+        h = self.relu3(self.bn3(self.conv3(h)))
+        h = self.relu4(self.bn4(self.conv4(h)))
+        h = self.pool2(h)
+
+        h = h.view(h.size(0), -1)
+        # h = self.fcmini(h)
+        h = self.fc1(h)
+
+        h = self.relu5(h)
+        h = self.drop(h)
+        y_pred = self.fc2(h)
+
+        return y_pred
+
+    # backward error propagation will be implemented by pytorch automatically
+    # so we don't need to define the error backpropagation function here
+
+    def train(self, X, y):
+        # check here for the torch.optim doc: https://pytorch.org/docs/stable/optim.html
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        # check here for the nn.CrossEntropyLoss doc: https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
+        loss_function = nn.CrossEntropyLoss()
+        # for training accuracy investigation purpose
+        accuracy_evaluator = Evaluate_Accuracy('training evaluator', '')
+
+        # it will be an iterative gradient updating process
+        # we don't do mini-batch, we use the whole input as one batch
+        # you can try to split X and y into smaller-sized batches by yourself
+        for epoch in range(self.max_epoch): # you can do an early stop if self.max_epoch is too much...
+            # get the output, we need to covert X into torch.tensor so pytorch algorithm can operate on it
+            y_pred = self.forward(torch.FloatTensor(np.array(X)))
+            # convert y to torch.tensor as well
+            y_true = torch.LongTensor(np.array(y))
+            # calculate the training loss
+            train_loss = loss_function(y_pred, y_true)
+
+            # check here for the gradient init doc: https://pytorch.org/docs/stable/generated/torch.optim.Optimizer.zero_grad.html
+            optimizer.zero_grad()
+            # check here for the loss.backward doc: https://pytorch.org/docs/stable/generated/torch.Tensor.backward.html
+            # do the error backpropagation to calculate the gradients
+            train_loss.backward()
+            # check here for the opti.step doc: https://pytorch.org/docs/stable/optim.html
+            # update the variables according to the optimizer and the gradients calculated by the above loss.backward function
+            optimizer.step()
+            accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
+            print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
+    
+    def test(self, X):
+        # do the testing, and result the result
+        y_pred = self.forward(torch.FloatTensor(np.array(X)))
+        # convert the probability distributions to the corresponding labels
+        # instances will get the labels corresponding to the largest probability
+        return y_pred.max(1)[1]
+    
+    def run(self):
+        print('method running...')
+        print('--start training...')
+        self.train(self.data['train']['X'], self.data['train']['y'])
+        print('--start testing...')
+        pred_y = self.test(self.data['test']['X'])
+        return {'pred_y': pred_y, 'true_y': self.data['test']['y']}
+            
--- script/stage_3_script/script_cifar.py
+++ script/stage_3_script/script_cifar.py
@@ -1,5 +1,5 @@
-from local_code.stage_3_code.Dataset_MNIST import Dataset_Loader
-from local_code.stage_3_code.Method_CNN_MNIST import Method_CNN_MNIST
+from local_code.stage_3_code.Dataset_CIFAR import Dataset_Loader
+from local_code.stage_3_code.Method_CNN_CIFAR import Method_CNN_CIFAR
 from local_code.stage_3_code.Result_Saver import Result_Saver
 from local_code.stage_3_code.Setting_Train_Test_Split import Setting_Train_Test_Split
 from local_code.stage_3_code.Evaluate_Accuracy import Evaluate_Accuracy
@@ -14,18 +14,17 @@ if 1:
     #------------------------------------------------------
 
     # ---- objection initialization setction ---------------
-    data_obj = Dataset_Loader('Mnist', '')
+    data_obj = Dataset_Loader('CIFAR', '')
     data_obj.dataset_source_folder_path = 'data/stage_3_data/'
-    data_obj.dataset_source_file_name = 'MNIST'
+    data_obj.dataset_source_file_name = 'CIFAR'
 
-    method_obj = Method_CNN_MNIST('CNN', '')
+    method_obj = Method_CNN_CIFAR('CNN', '')
 
     result_obj = Result_Saver('saver', '')
-    result_obj.result_destination_folder_path = 'result/stage_3_result/MNIST'
+    result_obj.result_destination_folder_path = 'result/stage_3_result/ORL'
     result_obj.result_destination_file_name = 'prediction_result'
 
-    setting_obj = Setting_Train_Test_Split('MNIST', '')
-
+    setting_obj = Setting_Train_Test_Split('CIFAR', '')
     evaluate_obj = Evaluate_Accuracy('accuracy', '')
     # ------------------------------------------------------
 
@@ -36,7 +35,7 @@ if 1:
     mean_score, std_score = setting_obj.load_run_save_evaluate()
     infodict = evaluate_obj.evaluate()
     print('************ Overall Performance ************')
-    print('CNN Accuracy for MNIST: ' + str(mean_score) + ' +/- ' + str(std_score))
+    print('CNN Accuracy for CIFAR: ' + str(mean_score) + ' +/- ' + str(std_score))
     print('************ Finish ************')
     # ------------------------------------------------------
     
